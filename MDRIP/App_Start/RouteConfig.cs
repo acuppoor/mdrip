e1e52cc17de2980ffd51fc26e4f9c80702f15024
Arjun K.S.S. Cuppoor <acuppoor@gmail.com>
2018-07-13 08:20:13 +0400

    Branch for Feasibility Demo Created

--- MDRIP/App_Start/RouteConfig.cs
+++ MDRIP/App_Start/RouteConfig.cs
@@ -18,6 +18,12 @@ namespace MDRIP
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
             );
+
+            routes.MapRoute(
+                name: "map",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Visualisation", action = "map", id = UrlParameter.Optional }
+            );
         }
     }
 }
