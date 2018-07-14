587b61dc86625b78028431ec8b2e3e1bdbdb90b5
Arjun K.S.S. Cuppoor <acuppoor@gmail.com>
2018-07-14 19:54:28 +0400

    Email confirmation when creating an account is working. Have to prevent user from getting logged in automatically when creating an account.

--- MDRIP/App_Start/IdentityConfig.cs
+++ MDRIP/App_Start/IdentityConfig.cs
@@ -14,6 +14,10 @@ using MDRIP.Models;
 using System.Net.Mail;
 using System.Diagnostics;
 using Twilio;
+using SendGrid.Helpers.Mail;
+using System.Net;
+using System.Configuration;
+using System.Net.Mime;
 
 namespace MDRIP
 {
@@ -26,21 +30,74 @@ namespace MDRIP
             SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", 587);
 
             String emailAdd = System.Configuration.ConfigurationManager.AppSettings["mailAccount"];
-
+            smtpClient.UseDefaultCredentials = false;
             smtpClient.Credentials = new System.Net.NetworkCredential(emailAdd, System.Configuration.ConfigurationManager.AppSettings["mailPassword"]);
-            smtpClient.UseDefaultCredentials = true;
             smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
             smtpClient.EnableSsl = true;
             MailMessage mail = new MailMessage();
-            mail.Body = message.Body;
+
+            String body = "Hi there, <br/>";
+            body += message.Body;
+            body += "<br/><br/>Thank you.";
+            mail.Body = body;
+
             mail.Subject = "MDRIP - DO NOT REPLY";
-            mail.To.Add(message.Destination);
+            mail.To.Add(new MailAddress(message.Destination));
             mail.From = new MailAddress(emailAdd, "MDRIP Admin");
             mail.To.Add(new MailAddress("acuppoor@gmail.com"));
             mail.CC.Add(new MailAddress(emailAdd));
+            mail.IsBodyHtml = true;
             smtpClient.Send(mail);
 
-            return Task.FromResult(0);
+
+            ///////////////////////////
+            //var myMessage = new SendGridMessage();
+            //myMessage.AddTo(message.Destination);
+            //myMessage.From = new SendGrid.Helpers.Mail.EmailAddress("honsmdrip@gmail.com", "No Reply - MDRIP");
+            //myMessage.Subject = message.Subject;
+            //myMessage.PlainTextContent = message.Body;
+            //myMessage.HtmlContent = message.Body;
+
+            //var credentials = new NetworkCredential(
+            //           ConfigurationManager.AppSettings["mailAccount"],
+            //           ConfigurationManager.AppSettings["mailPassword"]
+            //           );
+
+            //// Create a Web transport for sending email.
+            ////var transportWeb = new Web(credentials);
+
+            //#region formatter
+            //string text = string.Format("Please click on this link to {0}: {1}", message.Subject, message.Body);
+            //string html = "Please confirm your account by clicking this link: <a href=\"" + message.Body + "\">link</a><br/>";
+
+            //html += HttpUtility.HtmlEncode(@"Or click on the copy the following link on the browser:" + message.Body);
+            //#endregion
+
+            //MailMessage msg = new MailMessage();
+            //msg.From = new MailAddress("honsmdrip@gmail.com");
+            //msg.To.Add(new MailAddress(message.Destination));
+            //msg.Subject = message.Subject;
+            //msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(text, null, MediaTypeNames.Text.Plain));
+            //msg.AlternateViews.Add(AlternateView.CreateAlternateViewFromString(html, null, MediaTypeNames.Text.Html));
+
+            //SmtpClient smtpClient = new SmtpClient("smtp.gmail.com", Convert.ToInt32(587));
+            //smtpClient.UseDefaultCredentials = false;
+            //smtpClient.Credentials = credentials;
+            //smtpClient.EnableSsl = true;
+            //smtpClient.Send(msg);
+
+            // Send the email.
+            //if (transportWeb != null)
+            //{
+                //return transportWeb.DeliverAsync(myMessage);
+               
+            //}
+            //else
+            //{
+                return Task.FromResult(0);
+            //}
+
+            //return Task.FromResult(0);
         }
     }
 
