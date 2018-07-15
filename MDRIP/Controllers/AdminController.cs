6e61c95471b0825ae72020759effa99c2c25aac1
Arjun K.S.S. Cuppoor <acuppoor@gmail.com>
2018-07-15 21:04:00 +0400

    Admin can now Create and Read roles. Need to add Update and Delete role, and each account must be assosciated to one role only.

---
+++ MDRIP/Controllers/AdminController.cs
@@ -0,0 +1,77 @@
+﻿using MDRIP.Models;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.EntityFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MDRIP.Controllers
+{
+    public class AdminController : Controller
+    {
+        ApplicationDbContext context;
+
+        public AdminController()
+        {
+            context = new ApplicationDbContext();
+        }
+
+        public ActionResult Index()
+        {
+
+            if ((!User.Identity.IsAuthenticated) || (User.Identity.IsAuthenticated && !IsAdminUser()))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var Roles = context.Roles.ToList();
+            return View(Roles);
+        }
+
+        public Boolean IsAdminUser()
+        {
+            if (User.Identity.IsAuthenticated)
+            {
+                var user = User.Identity;
+                var UserManager = new UserManager<ApplicationUser>(new UserStore<ApplicationUser>(context));
+                var s = UserManager.GetRoles(user.GetUserId());
+                if (s[0].ToString() == "Admin")
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            return false;
+        }
+
+        public ActionResult Create()
+        {
+            if ((!User.Identity.IsAuthenticated) || (User.Identity.IsAuthenticated && !IsAdminUser()))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            var Role = new IdentityRole();
+            return View(Role);
+        }
+
+
+        [HttpPost]
+        public ActionResult Create(IdentityRole Role)
+        {
+            if ((!User.Identity.IsAuthenticated) || (User.Identity.IsAuthenticated && !IsAdminUser()))
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            context.Roles.Add(Role);
+            context.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
