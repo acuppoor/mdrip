e2a92e10caac4839da65a795a8ada22491986c4f
Arjun K.S.S. Cuppoor <acuppoor@gmail.com>
2018-07-26 13:24:41 +0400

    Upload csv/xls file started

--- MDRIP/Controllers/VisualisationController.cs
+++ MDRIP/Controllers/VisualisationController.cs
@@ -22,11 +22,11 @@ namespace MDRIP.Controllers
         {
             return View();
         }
-        //public ActionResult About()
-        //{
-        //    ViewBag.Message = "Your application description page.";
 
-        //    return View();
-        //}
+        public ActionResult Upload()
+        {
+
+            return View();
+        }
     }
 }
