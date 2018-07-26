b87b46a489567dee85ccae90144d2f76ebad7824
Arjun K.S.S. Cuppoor <acuppoor@gmail.com>
2018-07-26 14:39:30 +0400

    File can now be uploaded to assets folder

--- MDRIP/Controllers/VisualisationController.cs
+++ MDRIP/Controllers/VisualisationController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -22,11 +23,31 @@ namespace MDRIP.Controllers
         {
             return View();
         }
-        //public ActionResult About()
-        //{
-        //    ViewBag.Message = "Your application description page.";
+        public ActionResult Upload()
+        {
+            return View();
+        }
 
-        //    return View();
-        //}
+        [HttpPost]
+        public ActionResult Upload(HttpPostedFileBase file)
+        {
+            if (file != null && file.ContentLength > 0)
+                try
+                {
+                    string path = Path.Combine(Server.MapPath("~/Assets"),
+                                               Path.GetFileName(file.FileName));
+                    file.SaveAs(path);
+                    ViewBag.Message = "File uploaded successfully";
+                }
+                catch (Exception ex)
+                {
+                    ViewBag.Message = "ERROR:" + ex.Message.ToString();
+                }
+            else
+            {
+                ViewBag.Message = "You have not specified a file.";
+            }
+            return View();
+        }
     }
 }
