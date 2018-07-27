6e25b817f83652d3e059e86b239f924295d47097
Arjun K.S.S. Cuppoor <acuppoor@gmail.com>
2018-07-27 11:26:06 +0400

    ExcelReader helper class created that return the workbook. A Read method created in Visualisation controller that is temporarily and successfully reading the sheet.

--- MDRIP/Controllers/AdminController.cs
+++ MDRIP/Controllers/AdminController.cs
@@ -141,19 +141,5 @@ namespace MDRIP.Controllers
                 ModelState.AddModelError("", error);
             }
         }
-
-        private void ReadExcelOrCSV(String filepath){
-            string strConn = "Provider=Microsoft.Jet.OLEDB.4.0; " +"Data Source = "+ filepath +"; "+"Extended Properties = Excel 8.0; ";
-            OleDbConnection conn = new OleDbConnection(strConn);
-            conn.Open();
-            string strExcel = "";
-            OleDbDataAdapter myCommand = null;
-            DataSet ds = null;
-            strExcel = "select * from [sheet1$]";
-            myCommand = new OleDbDataAdapter(strExcel, strConn);
-            ds = new DataSet();
-            myCommand.Fill(ds, "table1");
-            //return ds;
-        }
     }
 }
--- MDRIP/Controllers/VisualisationController.cs
+++ MDRIP/Controllers/VisualisationController.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MDRIP.helper;
 
 namespace MDRIP.Controllers
 {
@@ -32,22 +33,51 @@ namespace MDRIP.Controllers
         public ActionResult Upload(HttpPostedFileBase file)
         {
             if (file != null && file.ContentLength > 0)
+            {
+                string filename = "";
+                string extension = "";
                 try
                 {
-                    string path = Path.Combine(Server.MapPath("~/Assets"),
-                                               Path.GetFileName(file.FileName));
-                    file.SaveAs(path);
-                    ViewBag.Message = "File uploaded successfully";
+                    filename = Path.GetFileName(file.FileName);
+                    int lastDotIndex = filename.LastIndexOf('.');
+                    extension = filename.Substring( lastDotIndex + 1, filename.Length - lastDotIndex-1);
+
+                    if (extension.Equals("xls") || extension.Equals("xlsx") || extension.Equals("csv"))
+                    {
+
+                        string path = Path.Combine(Server.MapPath("~/Assets"), filename);
+                        file.SaveAs(path);
+                        readfile(file.InputStream);
+                        ViewBag.Message += "." + extension + " file uploaded successfully";
+                    }
+                    else
+                    {
+                        ViewBag.Message = "Unknown file format: " + " " + filename ;
+                    }
                 }
                 catch (Exception ex)
                 {
                     ViewBag.Message = "ERROR:" + ex.Message.ToString();
                 }
+            }
             else
             {
                 ViewBag.Message = "You have not specified a file.";
             }
             return View();
+        }
+
+        private void readfile(Stream inputStream){
+            var excelReader = new ExcelReader(inputStream);
+
+            var items = excelReader.getData("Sheet1");
+
+            foreach (var v in items)
+            {
+                ViewBag.Message += v["Name"].ToString() + "  --  ";
+
+            }
+
         }
     }
 }
