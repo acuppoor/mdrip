477515d095ace6b48b4f0be447423dd78db08e66
takundac <tkchikondo@gmail.com>
2018-08-10 16:26:52 +0200

    made landing page the homepage and fixed the assets not found error

--- MDRIP/App_Start/RouteConfig.cs
+++ MDRIP/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace MDRIP
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Account", action = "LandingPage", id = UrlParameter.Optional }
             );
 
             routes.MapRoute(
